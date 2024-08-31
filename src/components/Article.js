2ea9fa3f53189c3cb1ba95f5a6ec398dbdcd7a15
Tevin <georgetevin0@gmail.com>
2024-08-31 16:39:33 +0300

    test

---
+++ src/components/Article.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Article({title, date = "January 1, 1970"  , preview}){
+    return(
+        <div>
+            <article>
+                <h3>{title}</h3>
+            </article>
+            <small>{date}</small>
+            <p>{preview}</p>
+        </div>
+    )
+}
+
+export default Article
